b6b549a42ee8581e6e0b9e61e46046f9b527bccb
Marco Mecchia <mmmecchi@amazon.com>
2021-02-19 19:34:26 -0300

    Exercise for next lesson

--- lesson1/ex6b.py
+++ lesson1/ex6b.py
@@ -1,8 +1,11 @@
 # 6.  A program that takes as input the quantity of flour expressed in grams and a percentage (e.g. a number between 0 and 100) that represents the quantity of water to use, and computes the water amount in grams
 
-x= float(input('Quantity of flour (g):'))
+x= input('Quantity of flour (g):')
 y= float(input('Hydratation degree (%):'))
-if y > 100 or y < 0:
-    print("Wrong value my friend")
-z=(x*y)/100
-print (z, 'Water amount (g)')
+if y >= 0 and y <= 100:
+    z=(x*y)/100
+    print (z, 'Water amount (g)')
+elif y > 100 and <= 200:
+    print("What exactly are you planning to do my friend")
+else:
+    print("You are a lost cause")
---
+++ lesson1/modulo.py
@@ -0,0 +1,6 @@
+x = int(input("Insert an integer number: "))
+
+if x%2 == 1:
+    print ('odd')
+else:
+    print ('even')
