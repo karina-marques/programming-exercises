e03be4166b90f68fb9f4217dade8e01bbbf309a3
Marco Mecchia <mmmecchi@amazon.com>
2021-02-22 09:38:34 -0300

    Add exercise8 for lesson 2

---
+++ lesson2/exercises/ex8.py
@@ -0,0 +1,16 @@
+# Write a program to accept a coordinate point in a XY coordinate system 
+# and determine in which quadrant the coordinate point lies. 
+# Quadrants are defined like this.                
+#                ^ y
+#                |
+#  Quadrant 2    |      Quadrant 1
+#                |
+#                |
+#                |
+#----------------------------------> x
+#                |
+#                |
+#                |
+#  Quadrant 3    |      Quadrant 4
+#                |
+#                v
