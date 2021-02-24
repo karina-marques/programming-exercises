f4866a8d6dd72bc679438522da4ea020c2317785
Marco Mecchia <mmmecchi@amazon.com>
2021-02-24 15:24:34 -0300

    Fix lesson 4

--- lesson4/practice/ex1.py
+++ lesson4/practice/ex1.py
@@ -1,8 +1,1 @@
-# Write a function that converts an input list containing
-# the numbers inside the list to Fahrneit. If the input_list
-# is null, convert the numbers from 0 to 100.
-
-def convert_celsius_temperatures_to_fahrneit(input_list = None):
-    if(input_list == None):
-        input_list = range(0, 100)
-    # Code goes here
+# Write one or more test cases for 'convert_temperatures'
--- lesson4/practice/ex2.py
+++ lesson4/practice/ex2.py
@@ -1,11 +1,1 @@
-# Write a function that counts the number of digits of an integer number given in input
-# Tip: our counting system is base 10. Use this to your advantage: what happens when you
-# divide a number by 10 multiple times?
-
-def count_digits(input_number):
-    number_of_digits = 0
-    # TODO: write code here
-    return number_of_digits
-
-n = int(input("Insert an integer number: "))
-print("The number of digits of", n, "is:", count_digits(n))
+# Write one or more test cases for 'count_digits'
